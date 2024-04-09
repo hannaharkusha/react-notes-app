ed2dde438e415bca810619901796f72977890b8a
Hanna Harkusha <aninsamsunk@gmail.com>
2024-04-09 14:01:34 +0200

    errors fixed

--- src/components/NoteOpened.js
+++ src/components/NoteOpened.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faTrash, faXmark} from "@fortawesome/free-solid-svg-icons";
-import Button from "./Button";
 
 function NoteOpened(props) {
 
